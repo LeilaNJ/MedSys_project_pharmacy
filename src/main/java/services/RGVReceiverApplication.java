fb608b321203802eefcdf36f6d6171119a4f0aac
davidaastrom <davida.astrom@gmail.com>
2021-05-21 08:59:35 +0200

    Working message sent

--- src/main/java/services/RGVReceiverApplication.java
+++ src/main/java/services/RGVReceiverApplication.java
@@ -46,6 +46,7 @@ public class RGVReceiverApplication implements ReceivingApplication<Message> {
     public RGVReceiverApplication() {
     }
 
+    /*
     @Override
     public Message processMessage(Message t, Map<String, Object> map) throws ReceivingApplicationException, HL7Exception {
         System.out.println("hej"); 
@@ -103,7 +104,7 @@ public class RGVReceiverApplication implements ReceivingApplication<Message> {
             pat_person.setLastname(lastName);
             personCtrl.create(pat_person);
         }
-        */
+        
         System.out.println("Patpers"); 
         
         Integer intidpatient = Integer.valueOf(idpatient);
@@ -126,7 +127,7 @@ public class RGVReceiverApplication implements ReceivingApplication<Message> {
             dr_person.setLastname(dr_lastName);
             personCtrl.create(dr_person);
         }
-*/
+
         System.out.println("Drpers");
         Integer intiddoctor = Integer.valueOf(iddoctor); 
         Doctor dduplicate = doctorCtrl.findDoctor(intiddoctor);
@@ -180,13 +181,114 @@ public class RGVReceiverApplication implements ReceivingApplication<Message> {
         System.out.println("Received message:\n" + encodedMessage + "\n\n");
         // Now generate a simple acknowledgment message and return it
         try {
+            System.out.println("hej");
         	return t.generateACK();
         } catch (IOException e) {
-            System.out.println("hej");
+            
             throw new HL7Exception(e);
         }
     }
+    */
 
+    @Override
+    public Message processMessage(Message t, Map<String, Object> map) throws ReceivingApplicationException, HL7Exception {
+        
+        // Recovery RGV_O01
+        RGV_O01 rgv = (RGV_O01) t;
+        
+        // Recovery PID Segment
+        PID pid = rgv.getPATIENT().getPID();
+        String idpatient = pid.getPatientIDInternalID(0).getID().getValue();
+        
+        // Recovery ORC Order
+                
+        ORC orc = rgv.getPATIENT().getORDER().getORC();
+        String idprescription = orc.getOrc1_OrderControl().getValue();
+        Date datepresc = orc.getOrc9_DateTimeOfTransaction().getTimeOfAnEvent().getValueAsDate();
+        String iddoctor = orc.getOrc10_EnteredBy().getIDNumber().getValue();
+        String signature = orc.getOrc10_EnteredBy().getGivenName().getValue();
+        Date deliverydate =orc.getOrc15_OrderEffectiveDateTime().getTimeOfAnEvent().getValueAsDate();
+        
+        // ORDER GIVE
+        
+        RXG rxg = rgv.getPATIENT().getORDER().getGIVE().getRXG();
+        String idmedicine = rxg.getGiveCode().getIdentifier().getValue();
+        String duration = rxg.getQuantityTiming().getDuration().getValue();
+        String intvadmin = rxg.getQuantityTiming().getInterval().getExplicitTimeInterval().getValue();
+        String dosage = rxg.getQuantityTiming().getQuantity().getQuantity().getValue();
+        String renewable = rxg.getQuantityTiming().getOrderSequencing().getCm_osd7_MaximumNumberOfRepeats().getValue();
+        String adminform = rxg.getGiveDosageForm().getText().getValue();
+        
+        // Add the values in the DB
+        
+        Prescription prescription = new Prescription();
+        
+        Integer intidprescription = Integer.valueOf(idprescription);
+        prescription.setIdprescription(intidprescription);
+        
+        Integer intidpatient = Integer.valueOf(idpatient);
+        Patient pduplicate = patientCtrl.findPatient(intidpatient);
+        if(pduplicate == null) {
+            Patient patient = new Patient();
+            patient.setIdpatient(intidpatient);
+            prescription.setPatient(patient);
+        }
+        else{
+            prescription.setPatient(pduplicate);
+        }
+
+        Integer intiddoctor = Integer.valueOf(iddoctor); 
+        Doctor dduplicate = doctorCtrl.findDoctor(intiddoctor);
+        if(dduplicate == null) {
+            Doctor doctor = new Doctor();
+            doctor.setIddoctor(intiddoctor);
+            prescription.setDoctor(doctor);
+        }
+        else {
+            prescription.setDoctor(dduplicate);
+        }
+        
+        Integer intidmedicine = Integer.valueOf(idmedicine);
+        Medicine mduplicate = medicineCtrl.findMedicine(intidmedicine);
+        if(mduplicate == null) {
+            Medicine medicine = new Medicine();
+            medicine.setIdmedicine(intidmedicine);
+            prescription.setMedicine(medicine);
+        }
+        else {
+            prescription.setMedicine(mduplicate);
+        }
+        
+        prescription.setSignature(signature);
+        
+        prescription.setDatePresc(datepresc);
+        
+        prescription.setDateDelivery(deliverydate);
+
+        Integer intdosage = Integer.valueOf(dosage);
+        prescription.setDosage(intdosage);
+        
+        prescription.setFreqAdministration(intvadmin);
+        
+        prescription.setDurationofTreatment(duration);
+        
+        prescription.setFormofAdministration(adminform);
+        
+        byte[] brenewable = renewable.getBytes();
+        prescription.setRenewable(brenewable);
+        
+        //Final messsage
+        
+        String encodedMessage = new DefaultHapiContext().getPipeParser().encode(t);
+        System.out.println("Received message:\n" + encodedMessage + "\n\n");
+        // Now generate a simple acknowledgment message and return it
+        try {
+        	return t.generateACK();
+        } catch (IOException e) {
+            throw new HL7Exception(e);
+        }
+    }
+    
     @Override
     public boolean canProcess(Message t) {
         return true;
--- src/main/java/viewer/PrescriptionReciever.java
+++ src/main/java/viewer/PrescriptionReciever.java
@@ -10,7 +10,6 @@ import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.app.HL7Service;
 import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.protocol.ReceivingApplication;
 import java.util.logging.Level;
 import java.util.logging.Logger; 
@@ -27,12 +26,10 @@ public class PrescriptionReciever {
         int port = 5647;
         HapiContext ctx = new DefaultHapiContext();
         server = ctx.newServer(port, false);
-        System.out.println("hallå");
         ReceivingApplication<Message> handler = new RGVReceiverApplication();
         server.registerApplication("RGV", "O01", handler);
         
         try {
-            System.out.println("hej"); 
             server.startAndWait();
         } catch (InterruptedException ex) {
             Logger.getLogger(HL7Services.class.getName()).log(Level.SEVERE, null, ex);
